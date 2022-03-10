5853cde45af41f121329958122d17164dcbcd916
devrawr <devrawr@protonmail.com>
2022-03-10 23:04:58 +0100

    make createConverter() method in Converters

--- src/main/kotlin/io/github/devrawr/rekt/convert/Converters.kt
+++ src/main/kotlin/io/github/devrawr/rekt/convert/Converters.kt
@@ -3,10 +3,11 @@ package io.github.devrawr.rekt.convert
 import io.github.devrawr.rekt.convert.impl.IntTypeConverter
 import io.github.devrawr.rekt.convert.impl.LongTypeConverter
 import io.github.devrawr.rekt.convert.impl.StringTypeConverter
+import java.lang.reflect.Type
 
 object Converters
 {
-    private val converters = hashMapOf<Class<*>, TypeConverter<*>>(
+    val converters = hashMapOf<Class<*>, TypeConverter<*>>(
         String::class.java to StringTypeConverter,
         Long::class.java to LongTypeConverter,
         Int::class.java to IntTypeConverter
@@ -23,5 +24,18 @@ object Converters
         {
             type
         }] as TypeConverter<T>?
+    }
+
+    inline fun <reified T : Any> createConverter(noinline convert: (ByteArray?) -> T?): TypeConverter<T>
+    {
+        return object : TypeConverter<T>
+        {
+            override fun convert(data: ByteArray?): T?
+            {
+                return convert.invoke(data)
+            }
+        }.apply {
+            converters[T::class.java] = this
+        }
     }
 }
